661d1b180afc0fccc16ff9243c528667fcf417af
crestFall3 <ewajenrola@gmail.com>
2026-05-20 12:00:23 +0100

    feat: update animation component for improved transitions

--- src/component/animation.jsx
+++ src/component/animation.jsx
@@ -4,9 +4,8 @@ export const Section = ({ children }) => {
   return (
     <motion.div
       initial={{ opacity: 0, y: 100 }}
-      whileInView={{ opacity: 1, y: 0 }}
+      animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      viewport={{ once: false, amount: 0.5 }}
     >
       {children}
     </motion.div>
